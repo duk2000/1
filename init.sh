d60a4334020342b88029557349defbe8d449ad61
duk2000 <92007210+duk2000@users.noreply.github.com>
2021-10-06 16:50:58 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,6 @@
-#nginx conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-#gunicorn conf
-sudo rm /etc/gunicorn.d/test
-sudo rm /etc/gunicorn.d/ask
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
 
 #database conf
 #sudo /etc/init.d/mysql restart
